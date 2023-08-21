b3c1a0f8d832472b3a5988a28045a5ab07ad11eb
Felipe Correia <felipedreamcorreia@hotmail.com>
2023-08-21 00:49:19 -0300

    feature: mapper filter

--- src/main/java/com/franciscode/filtroscombinados/mapper/FiltroContratosMapper.java
+++ src/main/java/com/franciscode/filtroscombinados/mapper/FiltroContratosMapper.java
@@ -6,7 +6,6 @@ import com.franciscode.filtroscombinados.viewModel.FiltroContratosRequestViewMod
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,33 +15,32 @@ public class FiltroContratosMapper {
 
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        List<ContratosResponseDTO> contratosFiltrados = responseDTOList.stream()
-                .filter(contrato -> requestViewModel.getCpfCnpj() == null || contrato.getCpfCnpj().equals(requestViewModel.getCpfCnpj()))
-                .filter(contrato -> requestViewModel.getIdContrato() == null || contrato.getIdContrato().equals(requestViewModel.getIdContrato()))
-                .filter(contrato -> requestViewModel.getDataPeriodoInicio() == null || contrato.getDataContratacao().compareTo(requestViewModel.getDataPeriodoInicio()) >= 0)
-                .filter(contrato -> requestViewModel.getDataPeriodoFim() == null || contrato.getDataContratacao().compareTo(requestViewModel.getDataPeriodoFim()) <= 0)
-                .filter(contrato -> requestViewModel.getDataVencimentoInicio() == null || contrato.getDataVencimento().compareTo(requestViewModel.getDataVencimentoInicio()) >= 0)
-                .filter(contrato -> requestViewModel.getDataVencimentoFim() == null || contrato.getDataVencimento().compareTo(requestViewModel.getDataVencimentoFim()) <= 0)
-                .filter(contrato -> requestViewModel.getValorInicio() == null || contrato.getValor().compareTo(requestViewModel.getValorInicio()) >= 0)
-                .filter(contrato -> requestViewModel.getValorFim() == null || contrato.getValor().compareTo(requestViewModel.getValorFim()) <= 0).toList();
+        return responseDTOList.stream()
+                .filter(contrato -> (requestViewModel.getCpfCnpj() == null || contrato.getCpfCnpj().equals(requestViewModel.getCpfCnpj())))
+                .filter(contrato -> (requestViewModel.getIdContrato() == null || contrato.getIdContrato().equals(requestViewModel.getIdContrato())))
+                .filter(contrato -> (requestViewModel.getDataPeriodoInicio() == null || contrato.getDataContratacao().compareTo(requestViewModel.getDataPeriodoInicio()) >= 0))
+                .filter(contrato -> (requestViewModel.getDataPeriodoFim() == null || contrato.getDataContratacao().compareTo(requestViewModel.getDataPeriodoFim()) <= 0))
+                .filter(contrato -> (requestViewModel.getDataVencimentoInicio() == null || contrato.getDataVencimento().compareTo(requestViewModel.getDataVencimentoInicio()) >= 0))
+                .filter(contrato -> (requestViewModel.getDataVencimentoFim() == null || contrato.getDataVencimento().compareTo(requestViewModel.getDataVencimentoFim()) <= 0))
+                .filter(contrato -> (requestViewModel.getValorInicio() == null || contrato.getValor().compareTo(requestViewModel.getValorInicio()) >= 0))
+                .filter(contrato -> (requestViewModel.getValorFim() == null || contrato.getValor().compareTo(requestViewModel.getValorFim()) <= 0))
+                .map(dto -> {
+                    ContratosResponseViewModel contratosResponseViewModel = new ContratosResponseViewModel();
+                    contratosResponseViewModel.setIdContrato(dto.getIdContrato());
+                    contratosResponseViewModel.setCpfCnpj(dto.getCpfCnpj());
+                    contratosResponseViewModel.setIndex(dto.getIndex());
+                    contratosResponseViewModel.setLiberacoes(dto.getLiberacoes());
+                    contratosResponseViewModel.setPercentual(dto.getPercentual());
+                    contratosResponseViewModel.setValor(dto.getValor());
+                    contratosResponseViewModel.setDataContratacao(dto.getDataContratacao());
+                    contratosResponseViewModel.setDataVencimento(dto.getDataVencimento());
+                    return contratosResponseViewModel;
+                })
+                .collect(Collectors.toList());
+    }
 
-
-        List<ContratosResponseViewModel> contratosResponseViewModelList = new ArrayList<>();
-
-        for (ContratosResponseDTO dto : contratosFiltrados) {
-            ContratosResponseViewModel contratosResponseViewModel = new ContratosResponseViewModel();
-            contratosResponseViewModel.setIdContrato(dto.getIdContrato());
-            contratosResponseViewModel.setCpfCnpj(dto.getCpfCnpj());
-            contratosResponseViewModel.setIndex(dto.getIndex());
-            contratosResponseViewModel.setLiberacoes(dto.getLiberacoes());
-            contratosResponseViewModel.setPercentual(dto.getPercentual());
-            contratosResponseViewModel.setValor(dto.getValor());
-            contratosResponseViewModel.setDataContratacao(dto.getDataContratacao());
-            contratosResponseViewModel.setDataVencimento(dto.getDataVencimento());
-
-            contratosResponseViewModelList.add(contratosResponseViewModel);
-        }
-
-        return contratosResponseViewModelList;
+    private static boolean isDateInRange(LocalDate date, LocalDate startDate, LocalDate endDate) {
+        return (startDate == null || date.isEqual(startDate) || date.isAfter(startDate)) &&
+                (endDate == null || date.isEqual(endDate) || date.isBefore(endDate));
     }
 }
